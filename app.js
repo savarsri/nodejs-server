c17f05e8eed2e598108fd54171cae31091890fad
AarushJ19 <aarushjain135@gmail.com>
2023-01-23 22:23:34 +0530

    routing

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+
+app.engine('html', require('ejs').renderFile);
+
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.render(__dirname + '/index.html')
+
 });
+
+app.get("/register.html", function(req, res){
+    res.render(__dirname + '/register.html')
+})
+
+// app.post('/register', function(req,res){
+//     res.render('register.html')
+// })
 app.listen(8080, function () {
     console.log("Server is running on localhost3000");
 });
