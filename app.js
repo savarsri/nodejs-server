263fa706ed190f4bb484a1b09653e426efb09157
AarushJ19 <aarushjain135@gmail.com>
2023-01-23 23:56:33 +0530

    authentication

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+// const { MongoUnexpectedServerResponseError } = require("mongodb");
 
 app.engine('html', require('ejs').renderFile);
+const saltRounds=10;
+const users=[]
+app.use(express.urlencoded({extended: false}))
 
-app.get("/", function (req, res) {
+app.post("/register.html", async(req,res)=>{
+
+    try{
+        // const hashedpassword= bcrypt.hash(req.body.password, (err,users))
+        bcrypt.hash(req.body.password, saltRounds, function(err, newpassword) {
+            // Store hash in database here
+            users.push({ 
+                id: Date.now().toString(),
+                email: req.body.email,
+                password:   ,
+            })
+            console.log(users); 
+            res.redirect("/adminlogin")
+        });
+    } catch(e){
+        console.log(e);
+        res.redirect('/register.html')
+    }
+
+})
+
+app.get("/adminlogin", function (req, res) {
     res.render(__dirname + '/index.html')
 
 });
+
 
 app.get("/register.html", function(req, res){
     res.render(__dirname + '/register.html')
 })
 
+
 // app.post('/register', function(req,res){
 //     res.render('register.html')
 // })
 app.listen(8080, function () {
-    console.log("Server is running on localhost3000");
+    console.log("Server is running on localhost:8080");
 });
