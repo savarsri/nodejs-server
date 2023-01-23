6be1c85ba0c28ab0ff540164e936af5f1c69d494
AarushJ19 <aarushjain135@gmail.com>
2023-01-24 00:16:25 +0530

    minor bug fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 // const { MongoUnexpectedServerResponseError } = require("mongodb");
 
 app.engine('html', require('ejs').renderFile);
