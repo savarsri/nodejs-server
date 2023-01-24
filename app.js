696be40c63a2e70ea453b8c83c12063d05f7fb37
AarushJ19 <aarushjain135@gmail.com>
2023-01-24 20:08:42 +0530

    jwt tokens

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcryptjs");
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 // const { MongoUnexpectedServerResponseError } = require("mongodb");
-
+let PORT = process.env.PORT || 8080;
 app.engine('html', require('ejs').renderFile);
+dotenv.config();
 const saltRounds=10;
 const users=[]
 app.use(express.urlencoded({extended: false}))
@@ -29,6 +32,45 @@ app.post("/register.html", async(req,res)=>{
 
 })
 
+app.post("/user/generateToken", (req, res) => {
+    // Validate User Here
+    // Then generate JWT Token
+  
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+        time: Date(),
+        userId: 12,
+    }
+  
+    const token = jwt.sign(data, jwtSecretKey);
+  
+    res.send(token);
+});
+  
+// Verification of JWT
+app.get("/user/validateToken", (req, res) => {
+    // Tokens are generally passed in header of request
+    // Due to security reasons.
+  
+    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  
+    try {
+        const token = req.header(tokenHeaderKey);
+  
+        const verified = jwt.verify(token, jwtSecretKey);
+        if(verified){
+            return res.send("Successfully Verified");
+        }else{
+            // Access Denied
+            return res.status(401).send(error);
+        }
+    } catch (error) {
+        // Access Denied
+        return res.status(401).send(error);
+    }
+});
+
 app.get("/adminlogin", function (req, res) {
     res.render(__dirname + '/index.html')
 
@@ -43,6 +85,6 @@ app.get("/register.html", function(req, res){
 // app.post('/register', function(req,res){
 //     res.render('register.html')
 // })
-app.listen(8080, function () {
+app.listen(PORT, function () {
     console.log("Server is running on localhost:8080");
 });
