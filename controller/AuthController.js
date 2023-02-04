e20f11cfffadf7e10e0658f7fd96d3cd65cb379c
Savar Srivastava <savarsri@gmail.com>
2023-02-04 15:38:53 +0530

    error handling

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -17,14 +17,16 @@ const register = (req, res, next) => {
         })
         user.save()
         .then(user => {
-            res.json({
+            res.status(200).json({
                 code: 200,
+                user,
                 message: "user added successfully"
             })
         })
         .catch(error => {
-            res.json({
+            res.status(500).json({
                 code: 406,
+                error,
                 message: 'error occured'
             })
         })
@@ -40,16 +42,15 @@ const login = (req, res, next) => {
     User.findOne({$or: [{email:username}, {phone:username}]})
     .then(user => {
         if(user){
-                    bcrypt.compare(password, user.password, function(err, result){
-                        if(err){
+                    bcrypt.compare(password, user.password, function(error, result){
+                        if(error){
                             res.json({
-                                error: err
+                                error
                             })
                         }
                         if(result){
                             let token = jwt.sign({name:user.name}, 'AzQPI!', {expiresIn: '1h'})
                             res.status(200).json({
-                                status:"ok",
                                 code:200,
                                 message: 'login successful',
                                 token});
@@ -68,10 +69,11 @@ const login = (req, res, next) => {
         }
     })
     .catch(error=>{
-        res.status(500).json(
-            {code: 500,
-            message: error}
-        )
+        res.status(500).json({
+            code: 500,
+            error,
+            message: "Try again"
+        })
     })
 }
 
