4a645a968517a4005a66b2d30e2ab7913aebef02
Savar Srivastava <savarsri@gmail.com>
2023-02-12 22:50:46 +0530

    Started Teams data

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-
 const EmployeeRoute = require('./routes/employee')
 const AuthRoute = require('./routes/auth')
 
